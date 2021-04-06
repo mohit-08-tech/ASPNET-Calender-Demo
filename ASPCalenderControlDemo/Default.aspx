43234f330d0960812cbf6350c1ce4dde027629ea
mohit-08-tech <mk082487200@gmail.com>
2021-04-06 21:26:58 +0530

    upload to git

---
+++ ASPCalenderControlDemo/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASPCalenderControlDemo.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            ASP .NET CALENDER CONTROL DEMO</div>
+        <asp:Calendar ID="Calendar1" runat="server" Height="245px" OnDayRender="Calendar1_DayRender" OnSelectionChanged="Calendar1_SelectionChanged" Width="293px"></asp:Calendar>
+        <p>
+            <asp:TextBox ID="txtText" runat="server" Width="241px"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ASPCalenderControlDemo/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPCalenderControlDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            txtText.Text = Calendar1.SelectedDate.ToString("dd/MM/yyyy");
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+           if(e.Day.IsToday)
+            {
+                e.Cell.BackColor = System.Drawing.Color.Aqua;
+                e.Cell.ForeColor = System.Drawing.Color.White;
+            }
+           if(e.Day.IsWeekend)
+            {
+                e.Cell.BackColor = System.Drawing.Color.Orange;
+                e.Cell.ForeColor = System.Drawing.Color.White;
+            }
+           if(e.Day.IsSelected)
+            {
+                e.Cell.BackColor = System.Drawing.Color.SeaGreen;
+            }
+        }
+    }
+}
